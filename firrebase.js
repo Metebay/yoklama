5be4b00400218bfdca8cfd3c6c60009a2bd06640
Metebay <bayindirnurettinm@gmail.com>
2024-11-18 14:48:50 +0300

    Update firebase.js

--- firrebase.js
+++ firrebase.js
@@ -2,16 +2,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
 
+// Firebase yapılandırma
 const firebaseConfig = {
-    apiKey: "AIzaSyDtw3dBVcjhftDX7KqGdpdfAH7rCZuSGM4",
-    authDomain: "yoklama-sistss.firebaseapp.com",
-    projectId: "yoklama-sistss",
-    storageBucket: "yoklama-sistss.firebasestorage.app",
-    messagingSenderId: "648200322291",
-    appId: "1:648200322291:web:5cbc32a11ca69caeb98b86",
-    measurementId: "G-13FDPVPXQW"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
+// Firebase uygulamasını başlat
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
